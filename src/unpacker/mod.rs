8adabe1ed27630211783d6888d51ef75cfa3ee9c
rrandom <emanonhere@gmail.com>
2024-10-11 15:01:57 +0800

    found entry

--- src/unpacker/mod.rs
+++ src/unpacker/mod.rs
@@ -1,25 +1,61 @@
 use oxc_ast::{
-    ast::{ArrayExpressionElement, Expression},
+    ast::{
+        ArrayExpressionElement, AssignmentExpression, Expression, MemberExpression, NumericLiteral,
+    },
     AstKind,
 };
-use oxc_semantic::Semantic;
+use oxc_semantic::{NodeId, Semantic};
 pub struct Module {}
 
 pub fn get_modules_form_webpack4(semantic: &Semantic) -> Option<Module> {
+    let mut entry_id = NodeId::DUMMY;
     for node in semantic.nodes().iter() {
-        if let AstKind::CallExpression(call) = node.kind() {
-            // println!("found Call {:?}", node.id());
-            if call.arguments.len() == 1 {
-                if let Expression::ArrayExpression(arr) = call.arguments[0].as_expression().unwrap() {
-                    let all_is_fun = arr.elements.iter().all(|d| {
-                        matches!(d, ArrayExpressionElement::FunctionExpression(_))
-                    });
-                    if all_is_fun {
-                        println!("Found? {:?}", node.id());
-                        // let span =  node.kind().as_call_expression().unwrap().span;
+        match node.kind() {
+            AstKind::CallExpression(call) => {
+                // println!("found Call {:?}", node.id());
+                if call.arguments.len() == 1 {
+                    if let Expression::ArrayExpression(arr) =
+                        call.arguments[0].as_expression().unwrap()
+                    {
+                        let all_is_fun = arr
+                            .elements
+                            .iter()
+                            .all(|d| matches!(d, ArrayExpressionElement::FunctionExpression(_)));
+                        if all_is_fun {
+                            println!("Found? {:?}", node.id());
+                            entry_id = node.id();
+                            // let span =  node.kind().as_call_expression().unwrap().span;
+                        }
                     }
                 }
             }
+            AstKind::AssignmentExpression(AssignmentExpression {
+                left,
+                right: Expression::NumericLiteral(s),
+                span,
+                ..
+            }) => {
+                let contains = semantic
+                    .nodes()
+                    .ancestors(node.id())
+                    .any(|id| id == entry_id);
+                if !contains {
+                  continue;
+                }
+                if let Some(MemberExpression::StaticMemberExpression(mm)) =
+                    left.as_member_expression()
+                {
+                    if mm.object.is_identifier_reference() && mm.property.name.as_str() == "s" {
+                        // println!("Member: {:?}", mm);
+                        println!(
+                            "assign: {} with {}",
+                            span.source_text(semantic.source_text()),
+                            s.value
+                        );
+                    }
+                }
+            }
+            _ => {}
         }
     }
 
