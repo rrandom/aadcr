66ffcbf1ad376fbf77c27a43eb4a3cad6eda9c7e
rrandom <emanonhere@gmail.com>
2024-11-14 12:30:30 +0800

    fix un_seq

--- crates/aadcr_unminify/src/passes/un_sequence_expr.rs
+++ crates/aadcr_unminify/src/passes/un_sequence_expr.rs
@@ -1,9 +1,23 @@
 use oxc_allocator::{CloneIn, Vec};
-use oxc_ast::ast::{Expression, Statement};
-use oxc_span::Span;
+use oxc_ast::ast::{Expression, ReturnStatement, Statement};
+use oxc_span::SPAN;
 use oxc_traverse::{Traverse, TraverseCtx};
 
 use super::UnminifyPass;
+
+trait ExpressionMut {
+    fn without_parentheses_mut(&mut self) -> &mut Self;
+}
+
+impl ExpressionMut for Expression<'_> {
+    fn without_parentheses_mut(&mut self) -> &mut Self {
+        let mut expr = self;
+        while let Expression::ParenthesizedExpression(paran_expr) = expr {
+            expr = &mut paran_expr.expression;
+        }
+        expr
+    }
+}
 
 /// Separate sequence expressions into multiple statements.
 ///  `a(), b(), c()` -> `a(); b(); c();`
@@ -32,128 +46,159 @@ impl<'a> Traverse<'a> for UnSequenceExpr {
         ctx: &mut TraverseCtx<'a>,
     ) {
         use oxc_ast::ast::AssignmentTarget::*;
-        let statements = &node.body.statements;
+        let statements = &mut node.body.statements;
         if statements.len() == 1 {
-            let statement = &statements[0];
+            let statement = statements.get_mut(0).unwrap();
             if let Statement::ExpressionStatement(expr) = statement
-                && let Expression::SequenceExpression(expr) = expr.expression.without_parentheses()
+                && let Expression::SequenceExpression(expr) =
+                    expr.expression.without_parentheses_mut()
             {
-                let exprs = &expr.expressions;
-                let mut body_statements = ctx.ast.vec_with_capacity(exprs.len() + 1);
-                if let Some((last, exprs)) = exprs.split_last() {
-                    for expr in exprs {
-                        body_statements.push(ctx.ast.statement_expression(
-                            Span::default(),
-                            expr.clone_in(ctx.ast.allocator),
-                        ));
-                    }
-                    if let Expression::AssignmentExpression(assign) = last {
-                        match &assign.left {
-                            AssignmentTargetIdentifier(left) => {
-                                body_statements.push(ctx.ast.statement_expression(
-                                    Span::default(),
-                                    last.clone_in(ctx.ast.allocator),
-                                ));
+                let exprs = &mut expr.expressions;
+                let len = exprs.len();
+                let mut insertion = ctx.ast.vec();
 
-                                body_statements.push(ctx.ast.statement_return(
-                                    Span::default(),
+                for expr in exprs.iter_mut().take(len - 1) {
+                    insertion.push(
+                        ctx.ast
+                            .statement_expression(SPAN, ctx.ast.move_expression(expr)),
+                    );
+                }
+
+                let last = exprs.last_mut().unwrap();
+
+                match last {
+                    Expression::AssignmentExpression(assign) => {
+                        match &mut assign.left {
+                            AssignmentTargetIdentifier(left) => {
+                                let ret_statement = ctx.ast.statement_return(
+                                    SPAN,
                                     Some(ctx.ast.expression_from_identifier_reference(
                                         left.clone_in(ctx.ast.allocator),
                                     )),
-                                ));
+                                );
+                                insertion.push(
+                                    ctx.ast
+                                        .statement_expression(SPAN, ctx.ast.move_expression(last)),
+                                );
+
+                                *statement = ret_statement;
                             }
                             StaticMemberExpression(left) => {
-                                body_statements.push(ctx.ast.statement_expression(
-                                    Span::default(),
-                                    last.clone_in(ctx.ast.allocator),
-                                ));
-                                body_statements.push(ctx.ast.statement_return(
-                                    Span::default(),
+                                let ret_statement = ctx.ast.statement_return(
+                                    SPAN,
                                     Some(ctx.ast.expression_member(
                                         ctx.ast.member_expression_from_static(
                                             left.clone_in(ctx.ast.allocator),
                                         ),
                                     )),
-                                ));
+                                );
+                                insertion.push(
+                                    ctx.ast
+                                        .statement_expression(SPAN, ctx.ast.move_expression(last)),
+                                );
+                                *statement = ret_statement;
                             }
                             _ => {}
-                        }
-                    } else {
-                        body_statements.push(ctx.ast.statement_return(
-                            Span::default(),
-                            Some(last.clone_in(ctx.ast.allocator)),
-                        ))
+                        };
                     }
-                    node.body.statements = body_statements;
-                    node.expression = false;
+                    _ => {
+                        *statement = ctx
+                            .ast
+                            .statement_return(SPAN, Some(ctx.ast.move_expression(last)));
+                    }
                 }
+                statements.splice(0..0, insertion);
+                node.expression = false;
+                self.changed = true;
             }
         }
     }
 
     fn exit_program(&mut self, node: &mut oxc_ast::ast::Program<'a>, ctx: &mut TraverseCtx<'a>) {
-        self.unfuse_statements(&mut node.body, ctx);
+        self.unsequence_statements(&mut node.body, ctx);
     }
 }
 
 impl<'a> UnSequenceExpr {
-    fn unfuse_statements(
+    fn unsequence_statements(
         &mut self,
         statements: &mut Vec<'a, Statement<'a>>,
         ctx: &mut TraverseCtx<'a>,
     ) {
         let mut i = 0;
         while i < statements.len() {
-            let mut replacement = ctx.ast.vec();
+            let statement = statements.get_mut(i).unwrap();
 
-            match &mut statements.get_mut(i).unwrap() {
-                Statement::ReturnStatement(ret)
-                    if let Some(expr) = ret.argument.as_ref()
-                        && let Expression::SequenceExpression(expr) =
-                            expr.without_parentheses() =>
-                {
-                    let exprs = expr.expressions.clone_in(ctx.ast.allocator);
-                    if let Some((last, exprs)) = exprs.split_last() {
-                        for expr in exprs {
-                            replacement.push(ctx.ast.statement_expression(
-                                Span::default(),
-                                expr.clone_in(ctx.ast.allocator),
-                            ));
-                        }
-                        replacement.push(ctx.ast.statement_return(
-                            Span::default(),
-                            Some(last.clone_in(ctx.ast.allocator)),
-                        ));
-                        statements.splice(i..i + 1, replacement);
-                        i += exprs.len();
-                        continue;
+            match statement {
+                Statement::ReturnStatement(ret) => {
+                    if let Some(insertion) = self.try_un_sequence_return(ret, ctx) {
+                        let len = insertion.len();
+                        statements.splice(i..i, insertion);
+                        i += len;
                     }
                 }
                 Statement::IfStatement(if_stmt) => {
-                    if let Expression::SequenceExpression(expr) = if_stmt
-                        .test
-                        .clone_in(ctx.ast.allocator)
-                        .without_parentheses()
-                    {
-                        let exprs = expr.expressions.clone_in(ctx.ast.allocator);
-                        if let Some((last, exprs)) = exprs.split_last() {
-                            if_stmt.test = last.clone_in(ctx.ast.allocator);
-                            for expr in exprs {
-                                replacement.push(ctx.ast.statement_expression(
-                                    Span::default(),
-                                    expr.clone_in(ctx.ast.allocator),
-                                ));
-                            }
-                            statements.splice(i..i, replacement);
-                            i += exprs.len();
-                            continue;
-                        }
+                    if let Some(insertion) = self.try_un_sequence_if_test(&mut if_stmt.test, ctx) {
+                        let len = insertion.len();
+                        statements.splice(i..i, insertion);
+                        i += len;
                     }
                 }
                 _ => {}
             }
             i += 1;
         }
+    }
+
+    // TO-DO
+    fn try_un_sequence_return(
+        &mut self,
+        ret: &mut ReturnStatement<'a>,
+        ctx: &mut TraverseCtx<'a>,
+    ) -> Option<Vec<'a, Statement<'a>>> {
+        if let Some(expr) = ret.argument.as_mut()
+            && let Expression::SequenceExpression(expr) = expr.without_parentheses()
+        {
+            let exprs = expr.expressions.clone_in(ctx.ast.allocator);
+            if let Some((last, exprs)) = exprs.split_last() {
+                let mut replacement = ctx.ast.vec();
+
+                for expr in exprs {
+                    replacement.push(
+                        ctx.ast
+                            .statement_expression(SPAN, expr.clone_in(ctx.ast.allocator)),
+                    );
+                }
+                ret.argument = Some(last.clone_in(ctx.ast.allocator));
+                return Some(replacement);
+            }
+        }
+        None
+    }
+
+    fn try_un_sequence_if_test(
+        &mut self,
+        test: &mut Expression<'a>,
+        ctx: &mut TraverseCtx<'a>,
+    ) -> Option<Vec<'a, Statement<'a>>> {
+        if let Expression::SequenceExpression(seq_expr) = test.without_parentheses_mut() {
+            let exprs = &mut seq_expr.expressions;
+            let mut insertion = ctx.ast.vec();
+            let len = exprs.len();
+
+            for expr in exprs.iter_mut().take(len - 1) {
+                insertion.push(
+                    ctx.ast
+                        .statement_expression(SPAN, ctx.ast.move_expression(expr)),
+                );
+            }
+            let last = exprs.last_mut().unwrap();
+
+            *test = ctx.ast.move_expression(last);
+
+            return Some(insertion);
+        }
+        None
     }
 }
 
