524e2202720059f6e01fb48d3ea7038b6fe3fa37
rrandom <emanonhere@gmail.com>
2024-11-19 14:38:09 +0800

    try benchmark

---
+++ crates/aadcr_unpacker/benches/unpacker_benchmark.rs
@@ -0,0 +1,34 @@
+// crates/aadcr_unpacker/benches/unpacker_benchmark.rs
+
+use aadcr_unpacker::*;
+use browserify::get_modules_form_browserify;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use oxc_allocator::Allocator;
+use oxc_parser::Parser;
+use oxc_semantic::SemanticBuilder;
+use oxc_span::SourceType;
+use std::path::Path;
+
+fn benchmark_browserify(c: &mut Criterion) {
+    let name = "tests/fixtures/browserify.js";
+    let path = Path::new(name);
+    let source_text = std::fs::read_to_string(path).expect("{name} not found");
+    let allocator = Allocator::default();
+    let source_type = SourceType::from_path(path).unwrap();
+    
+    c.bench_function("browserify_unpacker", |b| {
+        b.iter(|| {
+            let ret = Parser::new(&allocator, black_box(&source_text), source_type).parse();
+        
+            let program = ret.program;
+        
+            SemanticBuilder::new(&source_text)
+                .with_excess_capacity(2.0)
+                .build(&program);
+            let _ = get_modules_form_browserify(&allocator, &program);
+        })
+    });
+}
+
+criterion_group!(benches, benchmark_browserify);
+criterion_main!(benches);
